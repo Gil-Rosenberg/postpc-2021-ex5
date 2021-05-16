5b6888b9dec85e869abce42905f886e86f2f07bd
Gil Rosenberg <gil.rosenberg@mail.huji.ac.il>
2021-05-16 14:46:24 +0300

    first preparations for the ex

---
+++ app/src/main/java/exercise/android/reemh/todo_items/TodoItemAdapter.java
@@ -0,0 +1,49 @@
+package exercise.android.reemh.todo_items;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class TodoItemAdapter extends RecyclerView.Adapter<TodoItemViewHolder> {
+
+    /*
+    TODO:   - maybe put here private list of all todoItems
+            - setItems(new list) { clear current list, addAll new list, notifyDataSetChanged() -> to the recyclerView }
+     */
+
+    @NonNull
+    @Override
+
+    // here we create view holder
+    public TodoItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        /*
+         TODO:  context = parent.context (Access to Android resources)
+                view = LayoutInflater.from(context).inflate(R.layout.row_todo_item, parent, false)
+                return new TodoItemViewHolder(view)
+         */
+        return null;
+    }
+
+    @Override
+
+    // here we need to configure the view holder
+    public void onBindViewHolder(@NonNull TodoItemViewHolder holder, int position) {
+        /*
+        TODO:   todoItem = todoItemsList[todoItem]
+                holder.text.setText(todoItem.text)  // configure text
+                holder.img.setImageResource         // configure img (if we have one)
+                configure color etc.....
+         */
+    }
+
+    @Override
+
+    // recyclerView asks the adapter how much items it needs to render in total (including titles)
+    public int getItemCount() {
+        /*
+        TODO return todoItemsList.size()
+         */
+        return 0;
+    }
+}
---
+++ app/src/main/java/exercise/android/reemh/todo_items/TodoItemViewHolder.java
@@ -0,0 +1,13 @@
+package exercise.android.reemh.todo_items;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class TodoItemViewHolder extends RecyclerView.ViewHolder {
+    public TodoItemViewHolder(@NonNull View itemView) {    // View itemView = row_todo_item.xml
+        super(itemView);
+        // TODO put here itemView.findViewById() of all relevant sub-views in itemView
+    }
+}
